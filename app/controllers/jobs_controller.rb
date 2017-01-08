a573542a65299f19ee2d7b2f36a87fb72dec47f0
apple <chgpgf@gamil.com>
2017-01-08 16:54:22 +0800

    jobs#new/create/edit/update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,43 @@
 class JobsController < ApplicationController
 
   def index
-    @job = Job.all
+    @jobs = Job.all
   end
 
   def show
     @job = Job.find(params[:id])
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.upate(job_params)
+      redirect_to jobs_path
+    else
+      render :eidt
+    end
+  end
+
 
   private
 
-  def jobs_params
+  def job_params
     params.require(:job).permit(:title, :description)
   end
 end
