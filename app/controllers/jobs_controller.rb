2f86de9c0393163f34d80225a688ca31d3724802
apple <chgpgf@gamil.com>
2017-01-08 16:18:23 +0800

    implement job#show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,17 @@
 class JobsController < ApplicationController
+
+  def index
+    @job = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+
+  private
+
+  def jobs_params
+    params.require(:job).permit(:title, :description)
+  end
 end
